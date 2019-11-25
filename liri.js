f0db05703d9bd6a0f1ea7baf023007f1ee50d7fc
Dipti Katkar <dk.aj1990@gmail.com>
2019-11-25 16:16:09 -0600

    Consoled out information about movie

--- liri.js
+++ liri.js
@@ -21,17 +21,38 @@ function getMovieInfo(){
     axios.get(queryUrl).then(function(response){
         console.log(response);
 //         * Title of the movie.
-    console.log(`------------------------`);    
+    console.log(`------------------------------------------------------------------------`);    
     console.log(`Title: ${response.data.Title}`);
-    console.log(`------------------------`); 
+   // console.log(`------------------------`); 
 
 //   * Year the movie came out.
+console.log(`------------------------------------------------------------------------`);     
+    console.log(`Release Year: ${response.data.Year}`);
+   // console.log(`------------------------`);
 //   * IMDB Rating of the movie.
+console.log(`------------------------------------------------------------------------`);   
+    console.log(`IMDB Rating: ${response.data.imdbRating}`);
+  //  console.log(`------------------------`);
 //   * Rotten Tomatoes Rating of the movie.
+    //console.log(`------------------------`);    
+    //console.log(`Rotten Tomatoes: ${response.data.Title}`);
+    //console.log(`------------------------`);
 //   * Country where the movie was produced.
+console.log(`------------------------------------------------------------------------`);   
+    console.log(`Country: ${response.data.Country}`);
+  //  console.log(`------------------------`);
 //   * Language of the movie.
+console.log(`------------------------------------------------------------------------`);      
+    console.log(`Language: ${response.data.Language}`);
+  //  console.log(`------------------------`);
 //   * Plot of the movie.
+console.log(`------------------------------------------------------------------------`);     
+    console.log(`Plot: ${response.data.Plot}`);
+  //  console.log(`------------------------`);
 //   * Actors in the movie.
+console.log(`------------------------------------------------------------------------`);      
+    console.log(`Actors: ${response.data.Actors}`);
+    console.log(`------------------------------------------------------------------------`);   
 
     }).catch(function(error){
         console.log(error);
