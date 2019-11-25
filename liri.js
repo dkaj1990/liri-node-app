3f6680dae66fb9b9534f909a184c9480d21ee8fa
Dipti Katkar <dk.aj1990@gmail.com>
2019-11-25 15:40:08 -0600

    Added omdb api key to dotenv file & retrieved it from there

--- liri.js
+++ liri.js
@@ -7,19 +7,31 @@ const axios = require("axios");
  
 const operation = process.argv[2];
 let input = "Frozen";
-let queryUrl= `http://www.omdbapi.com/?t=${input}&y=&plot=short&apikey=trilogy`;
+let queryUrl;
 
 switch(operation){
-    case "movie-this":
-        
+    case "movie-this": 
+        queryUrl= `http://www.omdbapi.com/?t=${input}&y=&plot=short&apikey=${process.env.OMDB_API_KEY}`;
         getMovieInfo();
         break;
 }
 
 function getMovieInfo(){
 
-    axios.get(queryUrl).then(function(reposnse){
-        console.log(reposnse);
+    axios.get(queryUrl).then(function(response){
+        console.log(response);
+//         * Title of the movie.
+    console.log(`------------------------`);    
+    console.log(`Title: ${response.data.Title}`);
+    console.log(`------------------------`); 
+
+//   * Year the movie came out.
+//   * IMDB Rating of the movie.
+//   * Rotten Tomatoes Rating of the movie.
+//   * Country where the movie was produced.
+//   * Language of the movie.
+//   * Plot of the movie.
+//   * Actors in the movie.
 
     }).catch(function(error){
         console.log(error);
